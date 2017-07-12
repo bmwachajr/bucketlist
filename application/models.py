b32ad2b2271ee7a67f8c3325573c9ca6cb3e9d94
Benjamin Wacha <wacha@Benjamins-MacBook-Pro.local>
2017-07-12 13:23:06 +0300

    add models bucketlist and item

--- application/models.py
+++ application/models.py
@@ -16,3 +16,27 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+class bucketlist(db.Model):
+    id = db.Column(db.interger, primary_key=True)
+    Title = db.column(db.String(80))
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return '<bucketlist %r>' % self.title
+
+
+class item(db.Model):
+    id = db.Column(db.interger, primary_key=True)
+    title = db.column(db.STring(80))
+    decsription = db.Column(db.String(120))
+
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
+
+    def __repr__(self):
+        return '<item %r>' % self.title
