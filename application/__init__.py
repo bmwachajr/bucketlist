a0dcb62848993c05ab2874205b255b3b27bb17ef
Benjamin Wacha <wacha@Benjamins-MacBook-Pro.local>
2017-07-17 13:11:05 +0300

    add db initializer

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object("config')
+db = SQLAlchemy(app)
 
-from application import views
+from application import views, models
