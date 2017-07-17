ffbf966306062798012b48a78cb5f5354fa48d0c
Benjamin Wacha <wacha@Benjamins-MacBook-Pro.local>
2017-07-17 13:10:09 +0300

    add database configurations

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-SQLACLHEMY_DATABASE_URI = 'sqlitte:////tmp/test.db'
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLACLHEMY_DATABASE_URI = 'sqlitte:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repo')
