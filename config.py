295d491e9c62dfa0880ca2f39238a0c52fd800d9
Benjamin Wacha <wacha@Benjamins-MacBook-Pro.local>
2017-07-19 19:15:03 +0300

    add environment variables

--- config.py
+++ config.py
@@ -1,6 +1,45 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repo')
+
+class Config(object):
+    """ Default Cinfiguration """
+
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
+        os.path.join(basedir, '/databases/app.db')
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repo')
+
+
+class Development(Config):
+    """ Development Configuration """
+
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
+        os.path.join(basedir, '/databases/development.db')
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repo/development')
+
+
+class Testing(Config):
+    """ Testing Cinfiguration """
+
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
+        os.path.join(basedir, 'databases/testing.db')
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repo/testing')
+
+
+class Production(Config):
+    """ Production Cinfiguration """
+
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
+        os.path.join(basedir, 'databases/app.db')
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repo/production')
