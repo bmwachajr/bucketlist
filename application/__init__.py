3d9f217a7dabf309e0640291957a265ab398ebba
Benjamin Wacha <wacha@Benjamins-MacBook-Pro.local>
2017-07-20 19:23:51 +0300

    add api endpoints

--- application/__init__.py
+++ application/__init__.py
@@ -1,14 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api
 from config import environment_configuration
 
 # set application environment
 environment = 'development'
 
+
 # create the flask application
-app = Flask(__name__)
-app.config.from_object(environment_configuration[environment])
+def create_application(environment):
+    app = Flask(__name__)
+    app.config.from_object(environment_configuration[environment])
+    return app
+
+
+app = create_application(environment)
 db = SQLAlchemy(app)
+api = Api(app=app, prefix="/api/v1/")
 
 
-from application import views, models
+from application import views, models,  resource_urls
+resource_urls.load_urls(api)
