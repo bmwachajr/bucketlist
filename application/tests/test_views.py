5e4a72dec5c31293d7665e099511242e64ed452d
Benjamin Wacha <wacha@Benjamins-MacBook-Pro.local>
2017-07-25 19:47:53 +0300

    testcase for secondary views

---
+++ application/tests/test_views.py
@@ -0,0 +1,10 @@
+from .tests import BaseTest
+
+
+class TestViews(BaseTest):
+
+    def test_index(self):
+        """ test the index view """
+        url = '/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 209)
