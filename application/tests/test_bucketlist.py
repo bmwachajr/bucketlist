689b6eb177c7190a6ef566a55c3f012f4f6f964e
Benjamin Wacha <wacha@Benjamins-MacBook-Pro.local>
2017-07-31 09:31:49 +0300

    add test cases for creating a bucketlist

--- application/tests/test_bucketlist.py
+++ application/tests/test_bucketlist.py
@@ -9,11 +9,41 @@ class BucketlistTestCase(BaseTest):
 
     def test_create_bucketlist(self):
         """ test create new bucketlist """
-        pass
+        user = {'username': 'default_user', 'password': 'password'}
+        response = self.client.post('/auth/login', data=user)
+        self.assertEqual(response.status_code, 200)
+
+        # extract auth_token
+        user_auth = json.loads(response.data)
+        auth_token = user_auth['auth_token']
+        headers = {'auth_token': auth_token}
+        form = {'name': 'Trip to paris'}
+        url = '/bucketlists/'
+
+        # create bucket list
+        response = self.client.post(url, data=form, headers=headers)
+        self.assertEqual(response.status_code, 201)
+        self.assertIn('Successfully created bucketlist', response.data.decode())
 
     def test_unathorized_bucketlist_creation(self):
         """ test creating a bucketlist without being logged in """
-        pass
+        form = {'name': 'bucketlist while not loggedin'}
+        url = '/bucketlists/'
+
+        response = self.client.post(url, data=form)
+        self.assertEqual(response.status_code, 401)
+        self.assertIn('Forbidden Access', response.data.decode())
+
+    def test_create_bucketlist_invalid_token(self):
+        """ test create a bucketlist with an invalid token """
+        form = {'name': 'Bucketlist invalid token'}
+        headers = {'auth_token': ''}
+        url = '/bucketlists/'
+
+        # create bucket list
+        response = self.client.post(url, data=form, headers=headers)
+        self.assertEqual(response.status_code, 401)
+        self.assertIn('Forbidden Access', response.data.decode())
 
     def test_create_invalid_bucketlist(self):
         """ test creating bucketlist with empty data """
