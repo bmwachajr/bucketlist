0e91f95328f75260ecad09354c4b30d414b4fecc
Benjamin Wacha <wacha@Benjamins-MacBook-Pro.local>
2017-08-02 23:14:43 +0300

    update item test case

--- application/tests/test_item.py
+++ application/tests/test_item.py
@@ -5,28 +5,23 @@ from application import SECRET_KEY
 from .tests import BaseTest
 
 
-class BucketlistTestCase(BaseTest):
+class ItemsTestCase(BaseTest):
 
-    def Setup(self):
-        """ setup users to add bucketlists """
-        self.user = {'username': 'default_user', 'password': 'password'}
-        response = self.client.post('/auth/login', data=self.user)
+    def test_create_item_successfully(self):
+        """ test create an item succesfully """
+        user = {'username': 'default_user', 'password': 'password'}
+        response = self.client.post('/auth/login', data=user)
         self.assertEqual(response.status_code, 200)
 
         # extract auth_token
         user_auth = json.loads(response.data)
         auth_token = user_auth['auth_token']
-        self.headers = {'auth_token': auth_token}
+        headers = {'auth_token': auth_token}
 
-        self.post_url = '/bucketlists/2/items/'
-        self.get_url = '/bucketlists/2/items/1'
-        self.delete_url = '/bucketlists/2/items/1'
-
-    def test_create_bucketlist(self):
-        """ test create new item """
         form = {'description': 'A skinny deep into the ice waters'}
+        url = '/bucketlists/2/items/'
 
         # create an item
-        response = self.client.post(self.post_url, data=form, headers=self.headers)
-        self.assertEqual(response.status_code, 201)
-        self.assertIn('Successfully created item', response.data.decode())
+        post_response = self.client.post(url, data=form, headers=headers)
+        self.assertEqual(post_response.status_code, 201)
+        self.assertIn('Successfully created item', post_response.data.decode())
